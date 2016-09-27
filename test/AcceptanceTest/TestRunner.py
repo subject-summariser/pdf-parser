6529c84d5ef3bcf625b0520cd89c9a56eac700e3
DESKTOP-28JPNN6\jacob <jacobvorreiter@gmail.com>
2016-09-27 13:39:51 +1000

    Added acceptance debug runner

---
+++ test/AcceptanceTest/TestRunner.py
@@ -0,0 +1,9 @@
+#EASY-INSTALL-ENTRY-SCRIPT: 'behave==1.2.5','console_scripts','behave'
+__requires__ = 'behave==1.2.5'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == "__main__":
+    sys.exit(
+        load_entry_point('behave==1.2.5','console_scripts','behave')()
+    )
